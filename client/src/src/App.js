efe7e984cd0911757b1583bc4fd0cb795b9c3146
Ama Frema <frema90@gmail.com>
2023-02-27 20:05:23 -0500

    updated

---
+++ client/src/src/App.js
@@ -0,0 +1,13 @@
+import "App.css";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+function App() {
+  return (
+    <div className="App">
+      <Auth />
+      <Home />
+      <Profile />
+    </div>
+  );
+}
